4edc9cc7a53f5ba1f879885a52c3188e188ca4fc
Jacek Kurowski <jacek@4dl.pl>
2025-05-07 11:00:45 +0200

    Kopiowanie mieszkań

--- app/Http/Controllers/Front/Static/IndexController.php
+++ app/Http/Controllers/Front/Static/IndexController.php
@@ -28,37 +28,12 @@ class IndexController extends Controller
 
     public function pages($page)
     {
-        $allowedPages = ['polityka-prywatnosci', 'regulamin', 'kontakt'];
+        $allowedPages = ['polityka-prywatnosci', 'regulamin', 'kontakt', 'finansowanie'];
 
         if (in_array($page, $allowedPages)) {
             return view('front.static.'.$page);
         }
 
         abort(404); // Return a 404 error if the page is not allowed
-    }
-
-    public function store(Request $request)
-    {
-        try {
-            $validated = $request->validate([
-                'name' => 'required|string|max:255',
-                'surname' => 'required|string|max:255',
-                'phone' => 'required|string|max:255',
-                'email' => 'required|email|max:255'
-            ]);
-
-            $client = $this->clientRepository->createClient(
-                [
-                    'name' => $request->name,
-                    'lastname' => $request->surname,
-                    'phone' => $request->phone,
-                    'email' => $request->email,
-                    'ip' => $request->ip()
-                ],null, 1, 'iframe'
-            );
-        } catch (\Throwable $exception) {
-
-        }
-        return redirect()->back()->with('success', 'Dziękujemy za rejestracje w systemie!');
     }
 }
---
+++ resources/views/front/static/finansowanie.blade.php
@@ -0,0 +1,83 @@
+@extends('layouts.page')
+
+@section('meta_title', 'Kontakt')
+@isset($page->meta_title) @section('seo_title', $page->meta_title) @endisset
+@isset($page->meta_description) @section('seo_description', $page->meta_description) @endisset
+@section('content')
+    <div class="breadcrumbs">
+        <div class="container">
+            <div class="d-flex flex-wrap gap-1 row-gap-2">
+                <a href="/" class="breadcrumbs-link">Strona główna</a>
+                <span class="breadcrumbs-separator">/</span>
+                <span class="breadcrumbs-current">Finansowanie</span>
+            </div>
+        </div>
+    </div>
+
+    <main>
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="section-title mb-4">
+                            <div class="sub-section-title">
+                                <span>LOWIECKA 100</span>
+                            </div>
+                            <h2 class="mb-4">Kredyt hipoteczny z mFinanse</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-8">
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="atut">
+                                    <img src="https://placehold.co/125" alt="">
+                                    <p class="mt-4">Konsultacje i wsparcie eksperta <br>w całym procesie kredytowym</p>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="atut">
+                                    <img src="https://placehold.co/125" alt="">
+                                    <p class="mt-4">Wybór najkorzystniejszej oferty <br>spośród wielu banków</p>
+                                </div>
+                            </div>
+                            <div class="col-6 mt-4">
+                                <div class="atut">
+                                    <img src="https://placehold.co/125" alt="">
+                                    <p class="mt-4">Zbadanie zdolności kredytowej <br>i pomoc w analizie Twoich finansów</p>
+                                </div>
+                            </div>
+                            <div class="col-6 mt-4">
+                                <div class="atut">
+                                    <img src="https://placehold.co/125" alt="">
+                                    <p class="mt-4">Mobilność i elastyczność ekspertów <br>w całej Polsce, dogodne formy komunikacji</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="pt-0">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="section-title mb-4">
+                            <div class="sub-section-title">
+                                <span>MFINANSE</span>
+                            </div>
+                            <h2 class="mb-4">Umów się na konsultację <br>i uzyskaj wsparcie eksperta kredytowego</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 d-flex justify-content-center align-items-center">
+                        <a href="https://mfinanse.pl/oferta-indywidualna/kredyt-hipoteczny/" target="_blank" rel="nofollow"><img src="{{ asset('../images/mBank_logo_finanse.jpg') }}" alt="Logotyp mBank finanse"></a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+@endsection
+
