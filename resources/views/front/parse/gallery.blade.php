e0c8b4fd359716bbd848b75222812a871c09a36a
Jacek Kurowski <jacek@4dl.pl>
2025-05-09 11:43:58 +0200

    Sucharskiego w Inwestor

--- resources/views/front/parse/gallery.blade.php
+++ resources/views/front/parse/gallery.blade.php
@@ -4,7 +4,7 @@
         @foreach ($list as $p)
         <div class="col-12 col-sm-6 col-md-4 p-2">
             <div class="col-gallery-thumb">
-                <a href="/uploads/gallery/images/{{$p->file}}" class="glightbox" rel="gallery-1" title="">
+                <a href="/uploads/gallery/images/{{$p->file}}" class="swipebox" rel="gallery-1" title="">
                     <picture>
                         <source type="image/webp" srcset="{{asset('uploads/gallery/images/thumbs/webp/'.$p->file_webp) }}">
                         <source type="image/jpeg" srcset="{{asset('uploads/gallery/images/thumbs/'.$p->file) }}">
